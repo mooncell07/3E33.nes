98c80ec384a5d292f93e7ef7e2fe3655f00b351e
Nova <80042274+mooncell07@users.noreply.github.com>
2024-09-02 22:41:19 +0530

    refactor: Move BaseEmulator to InstructionTest

--- src/main/kotlin/com/mooncell07/cecc/tests/InstructionTest.kt
+++ src/main/kotlin/com/mooncell07/cecc/tests/InstructionTest.kt
@@ -35,6 +35,55 @@ data class Test(
     @SerializedName("cycles") val cycles: List<List<Any>>,
 )
 
+class DebugDevice : AbstractDevice() {
+    override val type = DT.DEBUG
+    override val size = 0xFFFF
+    override val base = 0x0000
+
+    private val area: UByteArray = UByteArray(0x10000) { 0u }
+    private var noCycle: Boolean = false
+
+    var cycles: MutableList<MutableList<Any>> = mutableListOf(mutableListOf(Any()))
+
+    fun stopLogging() {
+        noCycle = true
+    }
+
+    fun startLogging() {
+        noCycle = false
+    }
+
+    override fun read(address: UShort): UByte {
+        val result = area[address.toInt()]
+        if (!noCycle) {
+            cycles.add(mutableListOf(address.toDouble(), result.toDouble(), "read"))
+        }
+        return result
+    }
+
+    override fun write(
+        address: UShort,
+        data: UByte,
+    ) {
+        area[address.toInt()] = data
+        if (!noCycle) {
+            cycles.add(mutableListOf(address.toDouble(), data.toDouble(), "write"))
+        }
+    }
+}
+
+open class BaseEmulator {
+    private val clock = Clock()
+    val debugDevice = DebugDevice()
+    val bus = Bus(clock, debugDevice)
+    val cpu = CPU(bus)
+
+    init {
+        buildInstructionTable()
+        cpu.PC = 0xC000u
+    }
+}
+
 class InstructionTest(
     file: File,
 ) : BaseEmulator() {
